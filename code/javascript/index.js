13e62d7a75cc31caf54b386fb88642c1eae0656a
Gammett <Gammett@users.noreply.github.com>
2018-03-06 00:05:54 -0800

    update 03-16-2018
    late night update

---
+++ code/javascript/index.js
@@ -0,0 +1,11 @@
+function doFirst(){
+	document.write("Jesus is first in my life.  That is my goal.");
+}
+function doSecond(){
+	document.write("Jesus is the way the truth and the life.");
+}
+function start(){
+	doFirst();
+	doSecond();
+}
+start();
